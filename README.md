1a0b4e8dd17a72f25aff7f07cf6ff27cf3d00d86
Sam <kulkarni.samarth@yahoo.com>
2023-05-11 15:49:27 +0530

    Added some basic code
    Added a hailstone function that returns the number of steps required to reach a termination condition.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CPP-Practice
+
+A repository for practicing C++. Some of the projects done on here will be pulled from CMPSC 132 programs for the fall 2022 semester. Others will probably just be things that seemed interesting at the time.
+
+## Table of Contents
+More things to come here!
+- Main.cpp
+  - A file that contains the main function for the project. This is where the program will start.
+  - This file will be updated as more projects are added to the repository.
+
+## Learnings
+(Also to be updated as this project progresses)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 
+using namespace std;
+
+class MiscFunctions {
+public:
+    static string getUserInput() {
+        string userInput;
+        cout << "Enter a string: ";
+        cin >> userInput;
+        return userInput;
+    };
+
+    int hailstone(int num){
+        int count = 1;
+        while(num != 1){
+            if(num % 2 == 0){
+                num = num / 2;
+            }else{
+                num = (num * 3) + 1;
+            }
+            count++;
+        }
+        return count;
+    };
+};
+
+class Car {
+public:
+    string brand;
+    string model;
+    int year;
+
+    Car(string x, string y, int z) {
+        brand = x;
+        model = y;
+        year = z;
+    };
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    Car carObj1("BMW", "X5", 1999);
+//    Car carObj2("Ford", "Mustang", 1969);
+//    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << endl;
+//    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << endl;
+//    return 0;
+
+    MiscFunctions miscFunctions;
+    int num = miscFunctions.hailstone(6);
+    cout << num << endl;
     return 0;
+
+    /*MiscFunctions mainClass;
+    cout << "Hello, World!" << endl;
+    cout << mainClass.getUserInput() << endl;
+    return 0;
+     */
 }
+
+
+
